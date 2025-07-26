282dcc22030d1820f72481914fe8907ecc1dfacf
Alain <hafpixels06@gmail.com>
2025-07-26 16:48:11 +0200

    'initialization'

---
+++ src/main/java/com/Kappa_Marathon/Kappa/KappaApplication.java
@@ -0,0 +1,13 @@
+package com.Kappa_Marathon.Kappa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KappaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kappa_Marathon/Kappa/KappaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kappa_Marathon.Kappa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
